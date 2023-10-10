5fb2811bea4954c8e39585a2f92c454f48ae0270
Isleimar Oliveira <isleimar@gmail.com>
2023-10-10 16:17:48 -0300

    Refatorando Car Racing

--- car_racing.py
+++ car_racing.py
@@ -2,10 +2,18 @@ import gymnasium as gym
 
 env = gym.make("CarRacing-v2", render_mode="human")
 observation, info = env.reset()
+r_max, r_min = 0, 0
 for _ in range(1000):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
-    print ("Reward: ", reward, "steering: ", action)
+    r_max = r_max if (r_max > reward)  else reward 
+    r_min = r_min if (r_min < reward)  else reward
+    print ("Observation: ", observation)
+    print ("Reward: ", reward)
+    print ("Terminated: ", terminated)
+    print ("Truncated: ", truncated)
+    print ("Info: ", info)           
     if terminated or truncated:
-        observation, info = env.reset()
+        observation, info = env.reset()        
 env.close() 
+print("Mínimo: ", r_min, " Máximo: ", r_max)
